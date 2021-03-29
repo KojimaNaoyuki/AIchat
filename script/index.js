9e87af94185fcdfb08e1042d3006543bd8597b80
KojimaNaoyuki <programkoji213@gmail.com>
2021-03-29 23:53:32 +0900

    2021/03/29/2

---
+++ script/index.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('index.js');
+
+    document.querySelector('.drawer-menu-icon-burger').addEventListener('click', () => {
+        document.querySelector('.global-content').classList.toggle('drawer-menu-icon-burger-open');
+    });
+});

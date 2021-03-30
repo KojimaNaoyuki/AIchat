b249db4c5f31158e01d8d592ba93e518dd03f6f8
KojimaNaoyuki <programkoji213@gmail.com>
2021-03-30 16:46:07 +0900

    2021/03/30/2

--- script/index.js
+++ script/index.js
@@ -4,4 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('.drawer-menu-icon-burger').addEventListener('click', () => {
         document.querySelector('.global-content').classList.toggle('drawer-menu-icon-burger-open');
     });
+
+    document.querySelector('#goto-chat').addEventListener('click', () => {
+        window.location = '/chat.html';
+    })
 });
